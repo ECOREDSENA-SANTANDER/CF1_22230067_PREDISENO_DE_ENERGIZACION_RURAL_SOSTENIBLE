70835215f43c0fce2e02ef4e036eb603e218b44f
PABG1990 <pedro19908454@gmail.com>
2026-05-05 11:34:02 -0500

    Act

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -26,7 +26,7 @@
       .col-lg-12
         figure
           .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/eL2BJdqMNiY?si=kOU29JZ-BeZyqkZF" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/96mU7iEYc7U?si=2HgUocfwDMRiIkhl" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
         figcaption Video. Diagnóstico social y energético de usuario rural
 
 </template>
